cd85d51a6daf1f61f1305fb3f5aa74987014d549
BeckHelloWord! <yuwei_shiny@163.com>
2017-11-21 22:31:09 +0800

    first master

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathname){
-    console.log("About to route a request for "+ pathname);
+function route(handle, pathname,response) {
+  if (typeof handle[pathname] === 'function') {
+   return handle[pathname](response);
+  } else {
+    console.log("No request handler found for "+ pathname);
+    response.writeHead(404,{"Content-Type":"text/plain"});
+    response.write("404 Not found");
+    response.end();
   }
-  
-  exports.route = route;
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -2,16 +2,20 @@ var http = require("http");
 var url = require("url");
 
 
-function start(route) {
+function start(route,handle) {
     function onRequest(request, response) {
+        
         var pathname = url.parse(request.url).pathname;
+        if(pathname=='/favicon.ico'){
+            return false;
+        }
         console.log("Request for "+ pathname +" received.");
 
-        route(pathname);
+        route(handle,pathname,response);
 
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("index模块过来");
-        response.end();
+        // response.writeHead(200, { "Content-Type": "text/plain" });
+        // response.write(route(handle,pathname));
+        // response.end();
     }
 
     http.createServer(onRequest).listen(8888);
